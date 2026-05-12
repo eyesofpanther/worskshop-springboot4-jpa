f3157084b6ea2095269e8ddc6a4c6f3df90847d9
eyesofpanther <eyesofpanther@gmail.com>
2026-05-12 17:26:50 -0400

    Product

---
+++ src/main/java/com/alcateia/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.alcateia.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alcateia.course.entities.Product;
+import com.alcateia.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+        
+        @Autowired
+        private ProductRepository repository;
+
+        public List<Product> findAll() {
+            return repository.findAll();
+        }
+
+        public Product findById(Long id){
+            Optional<Product> obj = repository.findById(id);
+            return obj.get();
+        }
+
+}
