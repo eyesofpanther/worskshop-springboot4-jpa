bfccc6119497d4f74dff775693dcbf9f1479c007
eyesofpanther <eyesofpanther@gmail.com>
2026-05-12 16:58:12 -0400

    Category

---
+++ src/main/java/com/alcateia/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.alcateia.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alcateia.course.entities.Category;
+import com.alcateia.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+        
+        @Autowired
+        private CategoryRepository repository;
+
+        public List<Category> findAll() {
+            return repository.findAll();
+        }
+
+        public Category findById(Long id){
+            Optional<Category> obj = repository.findById(id);
+            return obj.get();
+        }
+
+}
